6064856b8a16599927189be240659781ef9b6582
suviwe <suvi.westerberg@metropolia.fi>
2025-02-23 14:59:54 +0200

    lisätty yhteystiedot-sivu ja responsiivinen navigointi

--- MyHealthDiary/src/main.js
+++ MyHealthDiary/src/main.js
@@ -1,27 +1,4 @@
-/*import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))*/
 
 const lowBmi = `BMI alle 18,5.Painoindeksisi osoittaa, että olet alipainoinen.  Tämä voi tarkoittaa, että painosi on terveydelle mahdollisesti liian alhainen. Alipaino voi lisätä riskiä tietyille terveysongelmille, kuten heikentyneelle vastustuskyvylle, luuston haurastumiselle ja ravitsemuksellisille puutoksille. Suosittelemme keskustelua terveydenhuollon ammattilaisen kanssa tilanteen arvioimiseksi `;
 
