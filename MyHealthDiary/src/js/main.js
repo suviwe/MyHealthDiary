6269d90918765480659cb135dd80fcbcd5416bc6
suviwe <suvi.westerberg@metropolia.fi>
2025-03-02 18:52:30 +0200

    yritetty lisätä päiväkirja formia

--- MyHealthDiary/src/js/main.js
+++ MyHealthDiary/src/js/main.js
@@ -1,4 +1,20 @@
 function toggleMenu() {
     let menu = document.querySelector(".nav-links");
     menu.classList.toggle("active");
-  }
+ }
+
+console.log("löytyykö nappi" , document.querySelector(".add-diary"));
+
+document.querySelector(".add-diary").addEventListener("click", function() {
+    window.location.href = "diary.html";
+});
+
+document.querySelector(".add-activity").addEventListener("click", function() {
+    window.location.href = "activity.html";
+});
+
+document.querySelector(".add-cycle").addEventListener("click", function() {
+    window.location.href = "menstrual.html";
+});
+
+
